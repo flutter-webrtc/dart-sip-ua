72bd7b9ebf73fa893082d47457ca381cdff341d9
CloudWebRTC <duanweiwei1982@gmail.com>
2019-08-27 22:50:32 +0800

    Add Exceptions.dart.

---
+++ lib/src/Exceptions.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+class ErrorImpl extends Error {
+  final JsonEncoder encoder = new JsonEncoder();
+  var code;
+  var name;
+  var parameter;
+  var value;
+  var message;
+  var status;
+}
+
+class ConfigurationError extends ErrorImpl {
+  ConfigurationError(parameter, [ value]) {
+    this.code = 1;
+    this.name = 'CONFIGURATION_ERROR';
+    this.parameter = parameter;
+    this.value = value;
+    this.message = (this.value == null)
+        ? 'Missing parameter: ${this.parameter}'
+        : 'Invalid value ${encoder.convert(this.value)} for parameter "${this.parameter}"';
+  }
+}
+
+class InvalidStateError extends ErrorImpl {
+  InvalidStateError(status) {
+    this.code = 2;
+    this.name = 'INVALID_STATE_ERROR';
+    this.status = status;
+    this.message = 'Invalid status: ${status}';
+  }
+}
+
+class NotSupportedError extends ErrorImpl {
+  NotSupportedError(message) {
+    this.code = 3;
+    this.name = 'NOT_SUPPORTED_ERROR';
+    this.message = message;
+  }
+}
+
+class NotReadyError extends ErrorImpl {
+  NotReadyError(message) {
+    this.code = 4;
+    this.name = 'NOT_READY_ERROR';
+    this.message = message;
+  }
+}
+
+class TypeError extends AssertionError {
+  TypeError(message) : super(message);
+}
