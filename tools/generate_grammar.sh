c69707fe7fb6862ef834af5d56795aca758dcd59
CloudWebRTC <duanweiwei1982@gmail.com>
2019-09-24 20:38:21 +0800

    Add grammar parser generate tool.

---
+++ tools/generate_grammar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ROOT_DIR=$(cd `dirname $0`/../; pwd)
+
+export PATH="/usr/local/opt/dart@1/bin:$PATH"
+
+dart --version
+cd $ROOT_DIR/lib/src
+dart $ROOT_DIR/tools/peg/bin/peg.dart general grammar.peg
