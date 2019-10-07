d2a8572c1ef840fdb355714fb984b5dd37ff223d
CloudWebRTC <duanweiwei1982@gmail.com>
2019-10-07 11:41:31 +0800

    Separate the ActionButton into a class file.

--- example/lib/src/callscreen.dart
+++ example/lib/src/callscreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_webrtc/webrtc.dart';
 import 'package:sip_ua/src/RTCSession.dart';
 import 'package:sip_ua/src/NameAddrHeader.dart';
 
+import 'widgets/action_button.dart';
 import 'sip_ua_helper.dart';
 
 class CallScreenWidget extends StatefulWidget {
@@ -442,71 +443,5 @@ class _MyCallScreenWidget extends State<CallScreenWidget> {
           child:
               Container(height: 210, width: 300, child: _buildActionButtons()),
         ));
-  }
-}
-
-class ActionButton extends StatefulWidget {
-  final String title;
-  final IconData icon;
-  final bool checked;
-  final Color fillColor;
-  final Function() onPressed;
-
-  const ActionButton(
-      {Key key,
-      this.title,
-      this.icon,
-      this.onPressed,
-      this.checked = false,
-      this.fillColor = null})
-      : super(key: key);
-
-  @override
-  _ActionButtonState createState() => _ActionButtonState();
-}
-
-class _ActionButtonState extends State<ActionButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        RawMaterialButton(
-          onPressed: widget.onPressed,
-          splashColor: widget.fillColor != null
-              ? widget.fillColor
-              : (widget.checked ? Colors.white : Colors.blue),
-          fillColor: widget.fillColor != null
-              ? widget.fillColor
-              : (widget.checked ? Colors.blue : Colors.white),
-          elevation: 10.0,
-          shape: CircleBorder(),
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Icon(
-              widget.icon,
-              size: 30.0,
-              color: widget.fillColor != null
-                  ? Colors.white
-                  : (widget.checked ? Colors.white : Colors.blue),
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-          child: Text(
-            widget.title,
-            style: TextStyle(
-              fontSize: 15.0,
-              color: widget.fillColor != null
-                  ? widget.fillColor
-                  : Colors.grey[500],
-            ),
-          ),
-        )
-      ],
-    );
   }
 }
