6882565b299e14a1fb9a68deaa9327fd7ccdc3c4
Robert Sutton <rsutton@noojee.com.au>
2019-10-21 11:30:05 +1100

    fix for playing dtmf tones

--- example/lib/src/callscreen.dart
+++ example/lib/src/callscreen.dart
@@ -235,7 +235,7 @@ class _MyCallScreenWidget extends State<CallScreenWidget> {
     this.setState(() {
       //_showNumPad = !_showNumPad;
     });
-    helper.sendDTMF('1');
+    helper.sendDTMF('4654');
   }
 
   void _toggleSpeaker() {
