b1623d4796b7391dea212bf1510d581e8c6a21fc
Redemption <paradzaigwira@gmail.com>
2022-03-11 09:15:21 +0200

    Scroll view added & Status bar brightness for Android + iOS

--- example/lib/src/about.dart
+++ example/lib/src/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AboutWidget extends StatelessWidget {
   @override
@@ -6,16 +7,31 @@ class AboutWidget extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text("About"),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            // Status bar color
+            statusBarColor: Colors.white, // For both Android + iOS
+
+            // Status bar brightness (optional)
+            statusBarIconBrightness:
+                Brightness.dark, // For Android (dark icons)
+            statusBarBrightness: Brightness.light, // For iOS (dark icons)
+          ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                      '\Github:\nhttps://github.com/cloudwebrtc/dart-sip-ua.git')
-                ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                        '\Github:\nhttps://github.com/cloudwebrtc/dart-sip-ua.git')
+                  ],
+                ),
               ),
             ),
           ),
