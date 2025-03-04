bb0f8e173f93f315248c5fd7855d01203ffaf6ac
Volodymyr B <volodymyr.b@didww.com>
2025-03-04 13:21:51 +0200

    extend configuration for ice gathering process

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -372,7 +372,10 @@ class SIPUAHelper extends EventManager {
         'iceTransportPolicy':
             (_uaSettings?.iceTransportPolicy ?? IceTransportPolicy.ALL)
                 .toParameterString(),
-        'iceServers': _uaSettings?.iceServers
+        'iceServers': _uaSettings?.iceServers,
+        'tcpCandidatePolicy':  (_uaSettings?.tcpCandidatePolicy ??
+            TcpCandidatePolicy.ENABLED).toParameterString(),
+        'iceCandidatePoolSize': _uaSettings?.iceCandidatePoolSize
       },
       'mediaConstraints': <String, dynamic>{
         'audio': true,
@@ -853,6 +856,22 @@ extension _IceTransportPolicyEncoding on IceTransportPolicy {
   }
 }
 
+enum TcpCandidatePolicy{
+  ENABLED,
+  DISABLED
+}
+
+extension _TcpCandidatePolicyEncoding on TcpCandidatePolicy {
+  String toParameterString() {
+    switch (this) {
+      case TcpCandidatePolicy.ENABLED:
+        return 'enabled';
+      case TcpCandidatePolicy.DISABLED:
+        return 'disabled';
+    }
+  }
+}
+
 class UaSettings {
   WebSocketSettings webSocketSettings = WebSocketSettings();
   TcpSocketSettings tcpSocketSettings = TcpSocketSettings();
@@ -917,6 +936,18 @@ class UaSettings {
   /// See [IceTransportPolicy] for possible values.
   /// Will default to [IceTransportPolicy.ALL] if not specified.
   IceTransportPolicy? iceTransportPolicy;
+
+  /// Allows to disable tcp candidates gathering
+  /// Will default to [TcpCandidatePolicy.ENABLED] if not specified.
+  TcpCandidatePolicy? tcpCandidatePolicy;
+
+  /// An unsigned 16-bit integer value which specifies the size of the prefetched
+  /// ICE candidate pool. The default value is 0 (meaning no candidate prefetching will occur).
+  /// You may find in some cases that connections can be established more quickly
+  /// by allowing the ICE agent to start fetching ICE candidates before you start
+  /// trying to connect, so that they're already available for inspection
+  /// when RTCPeerConnection.setLocalDescription() is called.
+  int iceCandidatePoolSize = 0;
 
   /// Controls which kind of messages are to be sent to keep a SIP session
   /// alive.
