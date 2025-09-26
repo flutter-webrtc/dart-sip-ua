2109dd271d568a4ad0ae983980b26f5b1521ca8e
Volsavr <babiyvladymyr@gmail.com>
2025-09-26 16:50:10 +0300

    Setting for call statistics logs (#549)
    * writing call stats to log
    
    * Update rtc_session.dart
    
    * fix code format
    
    ---------
    
    Co-authored-by: Volodymyr B <volodymyr.b@didww.com>

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -194,6 +194,7 @@ class SIPUAHelper extends EventManager {
         uaSettings.connectionRecoveryMinInterval;
     _settings.terminateOnAudioMediaPortZero =
         uaSettings.terminateOnMediaPortZero;
+    _settings.log_call_statistics = uaSettings.logCallStatistics;
 
     try {
       _ua = UA(_settings);
@@ -898,6 +899,9 @@ class UaSettings {
 
   /// Min interval between recovery connection, default 2 sec
   int connectionRecoveryMinInterval = 2;
+
+  /// Allows to write advanced call statistics in the log after the call ends
+  bool logCallStatistics = false;
 
   bool terminateOnMediaPortZero = false;
 
